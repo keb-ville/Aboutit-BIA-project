f5fafefff145fdc55922f6c78a63dd43c83b2e4b
Kirsty <kirsty.butterworth@northcoders.com>
2025-01-23 14:54:26 +0000

    add Nar Bar and first Button'

--- aboutit-app/app/layout.tsx
+++ aboutit-app/app/layout.tsx
@@ -1,11 +1,38 @@
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/NavBar";
+//import Providers from "./providers";
+//import { ClerkProvider } from "@clerk/nextjs";
+const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = {
+  title: "About It Draft",
+  description: "",
+};
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    // <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Navbar />
+        <main className="container py-10">{children}</main>
+      </body>
     </html>
+    // </ClerkProvider>
   );
 }
--- aboutit-app/app/page.tsx
+++ aboutit-app/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import Navbar from "@/components/navbar/NavBar";
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-2xl ">Welcome to About It</h1>
+    </div>
+  );
 }
+
+export default HomePage;
