f77da0d0bc06764a8aa1b8418dc0c435cfeceb25
nc-jodie-cheung <jodie.cheung@northcoders.com>
2025-01-27 19:00:58 +0000

    moved feedback button

--- aboutit-app/components/navbar/NavBar.tsx
+++ aboutit-app/components/navbar/NavBar.tsx
@@ -1,4 +1,3 @@
-import NavSearch from "./NavSearch";
 import LinksDropdown from "./LinksDropdown";
 // import DarkMode from "./DarkMode";
 import Logo from "./Logo";
@@ -6,10 +5,9 @@ import { Button } from "../ui/button";
 function Navbar() {
   return (
     <nav className="border-b">
-      <div className="container flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+      <div className="container flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-6">
         <Logo />
-        <NavSearch />
-        <Button variant="default" size="lg" className="capitalize m-8">
+        <Button variant="default" size="lg" className="capitalize m-8 ml-auto">
           Give feedback
         </Button>
         <div className="flex gap-4 items-center ">
