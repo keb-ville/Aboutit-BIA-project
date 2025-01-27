58ab8132e9f9812401e93d6acfe4930c8747036c
nc-jodie-cheung <jodie.cheung@northcoders.com>
2025-01-27 17:33:17 +0000

    add logo in nav bar

--- aboutit-app/components/navbar/Logo.tsx
+++ aboutit-app/components/navbar/Logo.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
+
 function Logo() {
-  return <div>Logo</div>;
+  return <Image src="/imagelogo.svg" alt="Logo" width={120} height={120} />;
 }
 
 export default Logo;
