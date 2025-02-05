fec7a18f49dd85b4cb72c1c670d63b54a1055c83
nc-jodie-cheung <jodie.cheung@northcoders.com>
2025-02-05 13:34:19 +0000

    add link to back to trending cfc

--- app/bia/page.tsx
+++ app/bia/page.tsx
@@ -1,9 +1,13 @@
 import Chatbox from "../../components/chat/ChatBox";
-
+import Link from "next/link";
 function SpeakToBiaPage() {
+  //need to work on the endpoints for the routes
   return (
     <div>
       <h2 className="flex justify-center text-lg">Speak to BIA</h2>
+      <Link href="/trending-cfc" className="underline">
+        ← Go to Trending Calls for Change
+      </Link>
       <div className="w-3/4 lg:w-1/2 md:w-1/3">
         <Chatbox />
       </div>
