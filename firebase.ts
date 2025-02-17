bd3528c36c9ae99092c72df20d7a4e536e9239bd
Kirsty <kirsty.butterworth@northcoders.com>
2025-02-17 21:29:02 +0000

    added firebase for feedback storage

--- firebase.ts
+++ firebase.ts
@@ -5,11 +5,19 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: "aboutit-project.firebasestorage.app",
-  messagingSenderId: "646405074352",
-  appId: "1:646405074352:web:1337af873c449719cd7a75",
-  measurementId: "G-T7R1731LN5",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // storageBucket: "aboutit-project.firebasestorage.app",
+  // messagingSenderId: "646405074352",
+  // appId: "1:646405074352:web:1337af873c449719cd7a75",
+  // measurementId: "G-T7R1731LN5",
 };
+
+console.log(
+  "Firebase Project ID:",
+  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
